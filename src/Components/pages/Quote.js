eedc296df5b1c5fabbf9ed1785c1a923337250bd
rejoice <rmjspecial2015@gmail.com>
2022-08-11 11:52:10 +0100

    add home, quote and nav file to page folder

---
+++ src/Components/pages/Quote.js
@@ -0,0 +1,16 @@
+import React from 'react';
+import '../styles/Quote.css';
+
+function quote() {
+  return (
+    <div className="quoteContainer">
+      <h1>Best Quotes of maths</h1>
+      <h4>
+        the only way to learn mathematics is to do maths.
+        maths gives hope that every problem has a solution.
+      </h4>
+    </div>
+  );
+}
+
+export default quote;
